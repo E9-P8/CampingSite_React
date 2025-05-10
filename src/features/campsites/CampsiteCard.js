6decc501cc47c67f5b2e30b8908f4ca510a999ce
Elisa98-hash <puorto.elisa@hotmail.com>
2025-05-10 20:02:21 +0800

    campsitesSlice

---
+++ src/features/campsites/CampsiteCard.js
@@ -0,0 +1,21 @@
+import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
+
+const CampsiteCard = ({ campsite }) => {
+  return (
+    <Card>
+      <CardImg
+        width="100%"
+        /*
+        src={props.campsite.image}
+        alt={props.campsite.name}*/
+        src={campsite.image}
+        alt={campsite.name}
+      />
+      <CardImgOverlay>
+        <CardTitle>{campsite.name}</CardTitle>
+      </CardImgOverlay>
+    </Card>
+  );
+};
+
+export default CampsiteCard;
