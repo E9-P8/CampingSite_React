8e3c2a33e0955192184dd8d5deb4bf1792e85ebe
Elisa98-hash <puorto.elisa@hotmail.com>
2025-05-17 12:25:23 +0800

    Homepage Display

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,15 @@
 // per scrivere la struttura in automatico usare rafce
 
 import React from "react";
+import { Container } from "reactstrap";
+import DisplayList from "../features/display/DisplayList";
 
 const HomePage = () => {
-  return <div>Home page</div>;
+  return (
+    <Container>
+      <DisplayList />
+    </Container>
+  );
 };
 
 export default HomePage;
