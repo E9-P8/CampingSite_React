ae37c559eefcd778655959b0bfb312fbecb206a5
Elisa98-hash <puorto.elisa@hotmail.com>
2025-05-27 12:16:05 +0800

    Added Partners

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage.js";
 import ContactPage from "./pages/ContactPage.js";
 import CampsiteDetailPage from "./pages/CampsiteDetailPage.js";
+import AboutPage from "./pages/AboutPage.js";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="contact" element={<ContactPage />} />
         <Route path="directory" element={<CampsitesDirectoryPage />} />
         <Route path="directory/:campsiteId" element={<CampsiteDetailPage />} />
+        <Route path="about" element={<AboutPage />} />
       </Routes>
       <Footer />
     </div>
